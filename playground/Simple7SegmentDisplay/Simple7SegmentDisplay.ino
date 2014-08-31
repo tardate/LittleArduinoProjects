42a25a7833086247ac2d805a4756eb8f7823aa56
Paul Gallagher <gallagher.paul@gmail.com>
2014-08-31 11:52:45 +0800

    refactored Simple7SegmentDisplay code with flashing dot (ooh!)

--- playground/Simple7SegmentDisplay/Simple7SegmentDisplay.ino
+++ playground/Simple7SegmentDisplay/Simple7SegmentDisplay.ino
@@ -6,18 +6,18 @@
 // Note that these patterns are for common cathode displays
 // For common anode displays, change the 1's to 0's and 0's to 1's
 // 1 = LED on, 0 = LED off, in this order:
-//                                    Arduino pin: 2,3,4,5,6,7,8
+//                    Arduino pin: 2,3,4,5,6,7,8
 byte seven_seg_digits[10][7] = { { 1,1,1,1,1,1,0 },  // = 0
-                                                           { 0,1,1,0,0,0,0 },  // = 1
-                                                           { 1,1,0,1,1,0,1 },  // = 2
-                                                           { 1,1,1,1,0,0,1 },  // = 3
-                                                           { 0,1,1,0,0,1,1 },  // = 4
-                                                           { 1,0,1,1,0,1,1 },  // = 5
-                                                           { 1,0,1,1,1,1,1 },  // = 6
-                                                           { 1,1,1,0,0,0,0 },  // = 7
-                                                           { 1,1,1,1,1,1,1 },  // = 8
-                                                           { 1,1,1,0,0,1,1 }   // = 9
-                                                           };
+                                 { 0,1,1,0,0,0,0 },  // = 1
+                                 { 1,1,0,1,1,0,1 },  // = 2
+                                 { 1,1,1,1,0,0,1 },  // = 3
+                                 { 0,1,1,0,0,1,1 },  // = 4
+                                 { 1,0,1,1,0,1,1 },  // = 5
+                                 { 1,0,1,1,1,1,1 },  // = 6
+                                 { 1,1,1,0,0,0,0 },  // = 7
+                                 { 1,1,1,1,1,1,1 },  // = 8
+                                 { 1,1,1,0,0,1,1 }   // = 9
+                               };
 
 void setup() {
   pinMode(2, OUTPUT);
@@ -28,14 +28,30 @@ void setup() {
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
-  writeDot(0);  // start with the "dot" off
+  clearDot();
+  Serial.begin(9600);
 }
 
 void writeDot(byte dot) {
   digitalWrite(9, dot);
 }
 
+void clearDot() {
+  writeDot(0);
+}
+
+void blinkDot(byte times) {
+  for (byte count = 1; count <= times * 2; count++) {
+    writeDot(count % 2);
+    delay(1000);
+  }
+}
+
+
+// set display to `digit` [0..9]
 void sevenSegWrite(byte digit) {
+  Serial.print("digit:");
+  Serial.println(digit);
   byte pin = 2;
   for (byte segCount = 0; segCount < 7; ++segCount) {
     digitalWrite(pin, seven_seg_digits[digit][segCount]);
@@ -44,9 +60,10 @@ void sevenSegWrite(byte digit) {
 }
 
 void loop() {
-  for (byte count = 10; count > 0; --count) {
-   delay(1000);
-   sevenSegWrite(count - 1);
+  // countdown 9,8,7..0
+  for (byte count = 10; count > 0; count--) {
+    sevenSegWrite(count - 1);
+    delay(1000);
   }
-  delay(4000);
+  blinkDot(4);
 }
