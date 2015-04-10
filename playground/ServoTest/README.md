5137ba342a4c15ec359ec9efb969f4169d76dc96
Paul Gallagher <gallagher.paul@gmail.com>
2015-04-11 02:29:44 +0800

    [ServoTest] extend capabilities and add notes on positioning accuracy

--- playground/ServoTest/README.md
+++ playground/ServoTest/README.md
@@ -7,8 +7,8 @@ Test the positioning accuracy of a servo motor driven by an Arduino
 I'm testing the control of a TowerPro SG90 9g Micro Servo.
 According the the [datasheet](http://datasheet.sparkgo.com.br/SG90Servo.pdf), this has a 180 degree sweep.
 
-ServoTest runs a simple positioning routine, progressively moving the servo thru 0,45,90,135,180 degrees
-to allow measurement of the positioning accuracy.
+ServoTest accepts a range of commands over the serial interface to perform various servo positioning tasks.
+See the spash screen when you open the console window for options.
 
 ### Cable connections
 
@@ -38,11 +38,18 @@ Specifically:
 * The step positioning between 0 and 180 under by about 15 degrees e.g. the request top position at 45 degrees results in an actual position of about 30 degrees.
 * I tried a few different SG90 units, and the all exhibit similar but not exactly identical behaviour.
 
-I suspect the positioning accurcy may be largely fixed by tuning the Servo pulse width settings. TODO: that's an investigation for another day..
-
 Here is my little positioning chart:
 
 ![Performance](./assets/SG90_default_positioning_accuracy.jpg?raw=true)
+
+I suspect the positioning accurcy may be largely fixed by tuning the Servo pulse width settings. TODO: that's an investigation for another day..
+
+### Performance - Refined
+
+So I came back to the issue of accuracy, with some interesting findings:
+
+* for the particular servo in hand, the min/max pulse widths needed some modification. Using `myservo.attach(SERVO_PIN,600,2320);` gives a much high degree of accuracy fo 0-180 degree sweep than with the default settings (544/2400).
+* the servo appear quite sensitive to power. If a battery is running out of juice, will start to get trembling in the positioning.
 
 ## Construction
 
--- playground/ServoTest/ServoTest.ino
+++ playground/ServoTest/ServoTest.ino
@@ -10,16 +10,62 @@
 
 #define SERVO_PIN 9
 #define STEP_DELAY 1000
+#define IDEAL_MIN_PW 600
+#define IDEAL_MAX_PW 2320
 
 Servo myservo;
 
 void setup() {
-  myservo.attach(SERVO_PIN);
-  centerServo();
+  Serial.begin(9600);
+  myservo.attach(SERVO_PIN,IDEAL_MIN_PW,IDEAL_MAX_PW);
+  printInstructions();
 }
 
 void loop() {
-  stepAroundCompass();
+  char ch;
+  int pos;
+  int new_min;
+  int new_max;
+  
+  while (Serial.available() > 0) {
+    ch = Serial.read();
+    switch(ch) {
+    case 'c':
+      Serial.println("Centering servo..");
+      centerServo();
+      break;
+    case 'l':
+      Serial.println("Servo left (180)..");
+      myservo.write(180);
+      break;
+    case 'r':
+      Serial.println("Servo right (0)..");
+      myservo.write(0);
+      break;
+    case 'a':
+      new_min = Serial.parseInt();
+      new_max = Serial.parseInt();
+      Serial.print("Reattaching server with min pulse width = ");
+      Serial.print(new_min);
+      Serial.print(" and max pulse width = ");
+      Serial.println(new_max);
+      myservo.detach();
+      myservo.attach(SERVO_PIN, new_min, new_max);
+      break;
+    case 'p':
+      pos = Serial.parseInt();
+      Serial.print("positioning servo to ");
+      Serial.println(pos);
+      myservo.write(pos);
+      break;
+    case '1':
+      stepAroundCompass();
+      break;
+    case '?':
+      printInstructions();
+      break;
+    }
+  }
 }
 
 void centerServo() {
@@ -33,4 +79,32 @@ void stepAroundCompass() {
     delay(STEP_DELAY);
   }
   delay(STEP_DELAY * 3);
+}
+
+void printInstructions() {
+  
+  Serial.println("\n\nServoTest commands:");
+  Serial.println("c : center servo");
+  Serial.println("l : servo left");
+  Serial.println("r : servo right");
+  Serial.println("p<int> : move servo to <int> degrees");
+  Serial.println("a <int> <int> : reattach servo with min/max pulse width");
+  Serial.println("1 : behaviour 1 - step around compass");
+  
+  Serial.print("\nmin pulse width:");
+  Serial.println(MIN_PULSE_WIDTH);
+  Serial.print("max pulse width:");
+  Serial.println(MAX_PULSE_WIDTH);
+  Serial.print("default pulse width:");
+  Serial.println(DEFAULT_PULSE_WIDTH);
+  Serial.print("refresh interval:");
+  Serial.println(REFRESH_INTERVAL);
+  Serial.print("Servo library version interval:");
+  Serial.println(Servo_VERSION);
+  
+  Serial.print("current read degrees:");
+  Serial.println(myservo.read());
+  Serial.print("current read microseconds:");
+  Serial.println(myservo.readMicroseconds());
+  Serial.println("\nEnter a command: ");
 }
