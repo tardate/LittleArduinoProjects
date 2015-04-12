58f7873fa5f37049e2cc2d333a3506adbacb08f9
Paul Gallagher <gallagher.paul@gmail.com>
2015-04-12 22:53:12 +0800

    [ServoTest] add continuous step option

--- playground/ServoTest/ServoTest.ino
+++ playground/ServoTest/ServoTest.ino
@@ -26,7 +26,7 @@ void loop() {
   int pos;
   int new_min;
   int new_max;
-  
+
   while (Serial.available() > 0) {
     ch = Serial.read();
     switch(ch) {
@@ -61,6 +61,9 @@ void loop() {
     case '1':
       stepAroundCompass();
       break;
+    case '2':
+      continuousStepAroundCompass();
+      break;
     case '?':
       printInstructions();
       break;
@@ -81,8 +84,13 @@ void stepAroundCompass() {
   delay(STEP_DELAY * 3);
 }
 
+void continuousStepAroundCompass() {
+  while(true) stepAroundCompass();
+}
+
+
 void printInstructions() {
-  
+
   Serial.println("\n\nServoTest commands:");
   Serial.println("c : center servo");
   Serial.println("l : servo left");
@@ -90,7 +98,8 @@ void printInstructions() {
   Serial.println("p<int> : move servo to <int> degrees");
   Serial.println("a <int> <int> : reattach servo with min/max pulse width");
   Serial.println("1 : behaviour 1 - step around compass");
-  
+  Serial.println("2 : behaviour 2 - continuous step sequence");
+
   Serial.print("\nmin pulse width:");
   Serial.println(MIN_PULSE_WIDTH);
   Serial.print("max pulse width:");
@@ -101,7 +110,7 @@ void printInstructions() {
   Serial.println(REFRESH_INTERVAL);
   Serial.print("Servo library version interval:");
   Serial.println(Servo_VERSION);
-  
+
   Serial.print("current read degrees:");
   Serial.println(myservo.read());
   Serial.print("current read microseconds:");
