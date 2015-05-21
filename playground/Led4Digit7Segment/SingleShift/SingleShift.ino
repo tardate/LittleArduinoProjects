207bdf1aa3732927c51430b2c1047a4b94c776d6
Paul Gallagher <gallagher.paul@gmail.com>
2015-05-22 00:32:31 +0800

    [4Digit7Segment/SingleShift] refactor code to interrupt-based refresh

--- playground/Led4Digit7Segment/SingleShift/SingleShift.ino
+++ playground/Led4Digit7Segment/SingleShift/SingleShift.ino
@@ -1,77 +1,81 @@
 /*
 
-  Simple4Digit7SegmentDisplay
+  SingleShift
   Drive a 4-Digit 7-Segment display with a 74HC595 shift register and 4 NPN BJTs.
 
-  For info and circuit diagrams see https://github.com/tardate/LittleArduinoProjects/tree/master/playground/Simple4Digit7SegmentDisplay
+  For info and circuit diagrams see https://github.com/tardate/LittleArduinoProjects/tree/master/playground/Led4Digit7Segment/SingleShift
 
  */
 
+// use timer for scheduling the display update
+#include <FlexiTimer2.h>
+
+// define pins for the shift register control
 #define ST_CP_LATCH_PIN 4
 #define SH_CP_CLOCK_PIN 5
 #define DS_DATA_PIN 3
+
+// define pins for the BJT control
 #define DIGIT_BASE_PIN 6
 #define DIGITS 4
 
-#define LCD_CLR 0b00000000
-#define LCD_DP  0b10000000
-#define LCD_0   0b00111111
-#define LCD_1   0b00000110
-#define LCD_2   0b01011011
-#define LCD_3   0b01001111
-#define LCD_4   0b01100110
-#define LCD_5   0b01101101
-#define LCD_6   0b01111101
-#define LCD_7   0b00000111
-#define LCD_8   0b01111111
-#define LCD_9   0b01101111
+// define the bitmasks for characters 0-9
+byte LCD_DIGIT_MASK[] = {
+  0b00111111,
+  0b00000110,
+  0b01011011,
+  0b01001111,
+  0b01100110,
+  0b01101101,
+  0b01111101,
+  0b00000111,
+  0b01111111,
+  0b01101111
+};
+
+volatile int number_for_display = 0;   // the number we'll display on the LED unit
+volatile byte current_digit = 0;       // 0-3 left to right
 
 void setup() {
+  Serial.begin(9600); 
   pinMode(ST_CP_LATCH_PIN, OUTPUT);
   pinMode(SH_CP_CLOCK_PIN, OUTPUT);
   pinMode(DS_DATA_PIN, OUTPUT);
   for(int p=0; p<DIGITS; p++) {
     pinMode(DIGIT_BASE_PIN + p, OUTPUT);
   }
+
+  FlexiTimer2::set(5, redrawLedDisplay); // update an LED character every 5ms
+  FlexiTimer2::start();
 }
 
 void loop() {
-  for(int p=0; p<DIGITS; p++) {
-    enableDigit(p);
-    registerWrite(LCD_0);
-    delay(200);
-    registerWrite(LCD_1);
-    delay(200);
-    registerWrite(LCD_2);
-    delay(200);
-    registerWrite(LCD_3);
-    delay(200);
-    registerWrite(LCD_4);
-    delay(200);
-    registerWrite(LCD_5);
-    delay(200);
-    registerWrite(LCD_6);
-    delay(200);
-    registerWrite(LCD_7);
-    delay(200);
-    registerWrite(LCD_8);
-    delay(200);
-    registerWrite(LCD_9);
-    delay(200);
-    registerWrite(LCD_CLR);
-    delay(200);
-  }
+  // increment a number every little while
+  delay(100);
+  if(number_for_display<9999) number_for_display += 1;
+  else number_for_display = 0;
 }
 
-// Command: send +data+ to the 74HC595 shift register
-void registerWrite(byte data) {
+
+void redrawLedDisplay() {
+  // stop glowing the current digit and move to the next
+  digitalWrite(DIGIT_BASE_PIN + current_digit, LOW);
+  current_digit++;
+  if(current_digit>3) current_digit = 0;
+
+  // do some gymnastics to figure out which number to display for this digit
+  int digit_scale = 1;
+  for(byte s=3; s>current_digit; s--) digit_scale = digit_scale * 10;
+  byte digit_value = (number_for_display / digit_scale) % 10;
+
+  // push the digit to the LED display
+  pushSegmentRegister(LCD_DIGIT_MASK[digit_value]);
+  digitalWrite(DIGIT_BASE_PIN + current_digit, HIGH);
+}
+
+// Command: send +data+ byte to the 74HC595 shift register
+void pushSegmentRegister(byte data) {
   digitalWrite(ST_CP_LATCH_PIN, LOW);
   shiftOut(DS_DATA_PIN, SH_CP_CLOCK_PIN, MSBFIRST, data);
   digitalWrite(ST_CP_LATCH_PIN, HIGH);
-}
-
-void enableDigit(byte digit) {
-  for(int p=0; p<DIGITS; p++) {
-    digitalWrite(DIGIT_BASE_PIN + p, digit == p);
-  }
 }
