b411279f32010c46ff82ddc69169e7602db24812
Paul Gallagher <gallagher.paul@gmail.com>
2016-06-23 02:24:57 +0800

    [catalog] fancy!

--- catalog/README.md
+++ catalog/README.md
@@ -10,3 +10,8 @@ I use two utility scripts:
 
 * [init_catalog_metadata.py](../bin/init_catalog_metadata.py) - used for the initial conversion from the table in the README.md
 * [update_catalog.py](../bin/update_catalog.py) - rebuilds the catalog.json when I add a project or change project metadata.
+
+## Credits and References
+* [bootstrap](http://getbootstrap.com)
+* [datatables](http://datatables.net/)
+
--- catalog/assets/javascripts/catalog.js
+++ catalog/assets/javascripts/catalog.js
@@ -8,12 +8,14 @@
       this.catalog_table = catalog_table;
       this.loadCatalog();
       this.github_base_url = 'https://github.com/tardate/LittleArduinoProjects/blob/master/';
+      this.pages_base_url = 'http://tardate.github.io/LittleArduinoProjects/';
     }
 
     CatalogController.prototype.loadCatalog = function() {
       var instance;
       instance = this;
       return this.catalog_table.DataTable({
+        autoWidth: false,
         ajax: {
           url: './catalog/catalog.json',
           dataSrc: ''
@@ -21,12 +23,12 @@
         columns: [
           {
             data: 'id',
-            width: "10%"
+            width: '10%'
           }, {
             data: 'name'
           }, {
             data: 'categories',
-            width: "20%"
+            visible: false
           }, {
             data: 'description',
             visible: false
@@ -34,12 +36,34 @@
         ],
         order: [[0, "desc"]],
         rowCallback: function(row, data, index) {
-          var full_name, id_link;
-          id_link = '<a href="' + instance.github_base_url + data.relative_path + '">' + data.id + '</a>';
-          $('td:eq(0)', row).html(id_link);
-          full_name = "<strong>" + data.name + "</strong>&nbsp;";
-          full_name += "<span>" + data.description + "</span>";
-          return $('td:eq(1)', row).html(full_name);
+          var base_name;
+          var id_link, id_cell;
+          var description, hero_image_url, category_array, category_labels;
+
+          base_name = data.relative_path.split('/').pop();
+          hero_image_url = instance.pages_base_url + data.relative_path + '/assets/' + base_name + '_build.jpg';
+          id_link = '<a href="' + instance.github_base_url + data.relative_path + '" class="leap-link">' + data.id + '</a>';
+          id_cell = $('td:eq(0)', row);
+          id_cell.html(id_link);
+
+          category_array = data.categories.split(',');
+          category_labels = '';
+          for (var cat = 0; cat < category_array.length; cat++) {
+            category_labels += '<span class="label label-primary">' + category_array[cat] + '</span> ';
+          }
+          description = '<div class="media"> \
+            <div class="media-body"> \
+              <h4 class="media-heading">' + data.name + '</h4> \
+              ' + data.description + ' \
+              <div>' + category_labels + '</div>  \
+            </div> \
+            <div class="media-right"> \
+              <a href="#"> \
+                <img class="media-object leap-hero" src="' + hero_image_url + '" alt=""> \
+              </a> \
+            </div> \
+          </div>';
+          return $('td:eq(1)', row).html(description);
         }
       });
     };
