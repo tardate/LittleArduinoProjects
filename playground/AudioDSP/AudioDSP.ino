5196f45fe4cd5b3e3fc6fdc03daa0debcad24fb2
Paul Gallagher <gallagher.paul@gmail.com>
2016-08-13 17:32:49 +0800

    [AudioDSP] add project

---
+++ playground/AudioDSP/AudioDSP.ino
@@ -0,0 +1,145 @@
+/*
+
+  AudioDSP
+  Simple clean boost/volume control example.
+  For info and circuit diagrams see https://github.com/tardate/LittleArduinoProjects/tree/master/playground/AudioDSP
+
+  Based on Electrosmash and OpenMusicLabs previous works:
+
+  CC-by-www.Electrosmash.com
+  Based on OpenMusicLabs previous works.
+  pedalshield_uno_booster.ino: pressing the pushbutton_1 or 2 turns the volume up or down.
+
+ */
+
+
+// defining harware resources.
+  #define LED 13
+  #define FOOTSWITCH 12
+  #define TOGGLE 2
+  #define PUSHBUTTON_1 A5
+  #define PUSHBUTTON_2 A4
+
+// defining the output PWM parameters
+  #define PWM_FREQ 0x00FF // pwm frequency - 31.3KHz
+  #define PWM_MODE 0 // Fast (1) or Phase Correct (0)
+  #define PWM_QTY 2 // 2 PWMs in parallel
+
+// other variables
+  int input, vol_variable=512;
+  int counter=0;
+  unsigned int ADC_low, ADC_high;
+
+
+void setup() {
+  // setup IO - inputs/outputs pins configurations and pull-ups
+  pinMode(FOOTSWITCH, INPUT_PULLUP);
+  pinMode(TOGGLE, INPUT_PULLUP);
+  pinMode(PUSHBUTTON_1, INPUT_PULLUP);
+  pinMode(PUSHBUTTON_2, INPUT_PULLUP);
+  pinMode(LED, OUTPUT);
+
+  // setup ADC - configured to be reading automatically the whole time.
+  ADMUX = 0x60;  /* ADC Multiplexer Selection Register = b01100000
+                  * REFS[1:0] = 01 = AVCC with external capacitor at AREF pin
+                  * ADLAR: ADC Left Adjust Result = 1
+                  * MUX[3:0]: Analog Channel Selection Bits = 0 = ADC0
+                  */
+  ADCSRA = 0xe5; /* ADC Control and Status Register A = b11100101
+                  * ADEN: ADC Enable
+                  * ADSC: ADC Start Conversion
+                  * ADATE: ADC Auto Trigger Enable
+                  * ADPS[2:0] ADC Prescaler = CLOCK/32
+                  */
+  ADCSRB = 0x07; /* ADC Control and Status Register B = b00000111
+                  * ADTS[2:0]: ADC Auto Trigger Source = 111 = Timer/Counter1 Capture Event
+                  */
+  DIDR0 = 0x01;  /* Digital Input Disable Register 0
+                  * ADC0D digital input buffer for adc0 disabled
+                  */
+
+  /*
+   * setup PWM - for more info about this config check the forum.
+   *
+   * PWM_MODE      | 0 | 1 |
+   * --------------|---|---|
+   * WGM13         | 1 | 1 |
+   * WGM12 (CTC1)  | 0 | 1 |
+   * WGM11 (PWM11) | 0 | 1 |
+   * WGM10 (PWM10) | 0 | 0 |
+   * Mode          | PWM, Phase and Frequency Correct | Fast PWM |
+   * TOP           | ICR1 | ICR1 |
+   */
+
+  /* TCCR1A: Timer/Counter1 Control Register A
+   * COM1A1:0: Compare Output Mode for Channel A = 10 = Clear OC1A on Compare Match
+   * COM1B1:0: Compare Output Mode for Channel B = (PWM_QTY - 1) << 5 = 10 for PWM_QTY = 2 = Clear OC1B on Compare Match
+   * WGM11:0: Waveform Generation Mode = PWM_MODE << 1
+   */
+  TCCR1A = (((PWM_QTY - 1) << 5) | 0x80 | (PWM_MODE << 1));
+
+  /* TCCR1B: Timer/Counter1 Control Register B
+   * WGM13: Waveform Generation Mode = 1
+   * WGM12: Waveform Generation Mode = PWM_MODE
+   * Clock Select = 1 = clk/1 (no prescaling)
+   */
+  TCCR1B = ((PWM_MODE << 3) | 0x11);
+
+  /* TIMSK1: Timer/Counter1 Interrupt Mask Register
+   * 0x20 = OCIE1A: Timer/Counter1, Output Compare A Match Interrupt Enable
+   */
+  TIMSK1 = 0x20;
+
+  /* ICR1H and ICR1L: Input Capture Register 1
+   * The Input Capture is updated with the counter (TCNT1) value each time an event occurs on the ICP1 pin (or optionally on the Analog  * Comparator output for Timer/Counter1). The Input Capture can be used for defining the counter TOP value.
+   */
+  ICR1H = (PWM_FREQ >> 8);
+  ICR1L = (PWM_FREQ & 0xff);
+
+  /* DDRB: The Port B Data Direction Register
+   */
+  DDRB |= ((PWM_QTY << 1) | 0x02); // turn on outputs
+  sei(); // turn on interrupts - not really necessary with arduino
+
+}
+
+
+void loop() {
+  // Turn on the LED if the effect is ON.
+  if (digitalRead(FOOTSWITCH)) digitalWrite(LED, HIGH);
+  else digitalWrite(LED, LOW);
+}
+
+
+// Timer 1 interrupt service routine
+ISR(TIMER1_CAPT_vect) {
+
+  // read the ADC input signal data: 2 bytes Low and High.
+  // Low byte needs to be fetched first
+  ADC_low = ADCL;
+  ADC_high = ADCH;
+
+  // construct the input sample summing the ADC low and high byte.
+  input = ((ADC_high << 8) | ADC_low) + 0x8000; // make a signed 16b value
+
+  // The push-buttons are checked now:
+  counter++; //to save resources, the push-buttons are checked every 100 times.
+  if(counter==100) {
+    counter=0;
+    if (!digitalRead(PUSHBUTTON_1)) {
+      // increase the vol
+      if (vol_variable<1024) vol_variable=vol_variable+1;
+    }
+    if (!digitalRead(PUSHBUTTON_2)) {
+      // decrease vol
+      if (vol_variable>0) vol_variable=vol_variable-1;
+    }
+  }
+
+  // the amplitude of the signal is modified following the vol_variable
+  input = map(input, 0, 1024, 0, vol_variable);
+
+  // write the PWM output signal
+  OCR1AL = ((input + 0x8000) >> 8); // convert to unsigned, send out high byte
+  OCR1BL = input; // send out low byte
+}
---
+++ playground/AudioDSP/README.md
@@ -0,0 +1,113 @@
+# AudioDSP
+
+Build and test an arduino DSP circuit based on the pedalSHIELD by electrosmash.
+
+![The Build](./assets/AudioDSP_build.jpg?raw=true)
+
+## Notes
+
+I've wanted to try some digital signal processing on the Arduino/AVR for a while.
+When I found the pedalSHIELD by electrosmash, it seemed like an excellent way to bootstrap some experimentation.
+
+This project is a breadboard-build of a circuit based on the pedalSHIELD, tested with a simple boost/volume control
+circuit based on the pedalSHIELD examples.
+
+
+### Input Processing
+
+I first built the circuit according to the pedalSHIELD schematics, but found that the input chain was prone to
+clipping and distorting a guitar input signal. I'd prefer a clean signal in, so that any effects are due
+to the DSP algorithms and later stages, so I made a few modifications:
+
+* adjusted to ~50kΩ input impedance
+* biased the signal to a DC offset of ~ VCC/2
+* removed the 6.8nF capacitor/filter
+* changed R4 from 4.7kΩ to 10kΩ to avoid clipping at minimum attenuation (RV1)
+
+
+### Input/Output Gain Stage
+
+The most suitable OpAmp I had on hand was an LMC6482 CMOS Dual Rail-To-Rail Input and Output Operational Amplifier.
+This prove to work just fine, and with very low power consumption too - all the external circuitry to the Arduino is
+only drawing about 0.5mA in full operation.
+
+
+### Output Processing
+
+Little changed from the original pedalSHIELD design at this point.
+
+
+### Code
+
+[AudioDSP.ino](./AudioDSP.ino) is a simple clean boost/volume control example from the pedalSHIELD examples.
+The pushbuttons control the output volume up and down.
+
+Core algorithm:
+
+* the main loop sets the effect on/off indicator (LED on pin 13)
+* input read/output write is handled in the Timer 1 interrupt service routine
+
+
+### Performance
+
+Bottom line:
+
+* it works! It's pretty amazing to jam away in the knowledge that the singal from the guitar is passing through an Arduino before getting to the amp
+* performance starts to drops off significantly from about 4kHz and by 8kHz it is down in the noise.
+
+I'd like to think it is possible to get better higher frequency performance from the circuit - ideally up to 20kHz -
+but that needs a bit more investigation. The processor should be capable, but I think the limitation is in the
+PWM signal generation and mixing/filtering in the output stage.
+
+In the following scope traces:
+
+* CH1 - the input signal (FX in)
+* CH2 - the final output signal (FX out)
+
+Here's a trace of a 1kHz 200mV signal at minimum input attenuation and default (mid-point) volume boost:
+
+![sine_1k](./sine_1k.gif?raw=true)
+
+And in the FFT of the output there's a strong fundamental:
+
+![sine_1k_fft](./sine_1k_fft.gif?raw=true)
+
+At 2kHz the signal is attenuated:
+
+![sine_2k](./sine_2k.gif?raw=true)
+
+And the 2kHz output FFT shows more noise and harmonics
+
+![sine_2k_fft](./sine_2k_fft.gif?raw=true)
+
+At 4kHz the signal is significantly attenuated:
+
+![sine_4k](./sine_4k.gif?raw=true)
+
+And the 4kHz output FFT shows more noise and harmonics
+
+![sine_4k_fft](./sine_4k_fft.gif?raw=true)
+
+By 8kHz the signal is pretty much destroyed:
+
+![sine_8k](./sine_8k.gif?raw=true)
+
+And the 8kHz output FFT shows the signal pretty much drowned by noise and harmonics
+
+![sine_8k_fft](./sine_8k_fft.gif?raw=true)
+
+
+## Construction
+
+![Breadboard](./assetsAudioDSP_bb.jpg?raw=true)
+
+![The Schematic](./assets/AudioDSP_schematic.jpg?raw=true)
+
+![The Build](./assets/AudioDSP_build.jpg?raw=true)
+
+## Credits and References
+* [pedalSHIELD UNO Arduino Guitar Pedal](http://www.electrosmash.com/pedalshield-uno)
+* [pedalSHIELD UNO @ arduino.cc](https://create.arduino.cc/projecthub/electrosmash/arduino-uno-guitar-pedal-b2ba96)
+* [LMC6482 datasheet](http://www.futurlec.com/Linear/LMC6482IN.shtml)
+* [Configure Arduino UNO PWM outputs to play audio](http://www.electrosmash.com/forum/pedalshield-uno/111-configure-arduino-uno-pwm-outputs-to-play-audio?lang=en)
+* [PWM Distortion Analysis](http://www.openmusiclabs.com/learning/digital/pwm-dac/pwm-distortion-analysis/index.html)
