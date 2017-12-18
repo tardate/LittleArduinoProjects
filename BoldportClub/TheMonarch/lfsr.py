1e8c0301dbea3ca4900dc5e4081e558857deb019
Paul Gallagher <gallagher.paul@gmail.com>
2017-12-19 00:58:51 +0800

    [monarch] fix the LFSR and schematic

--- BoldportClub/TheMonarch/lfsr.py
+++ BoldportClub/TheMonarch/lfsr.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 """ Simulate The Monarch's LFSR function.
 
-The Monarch has taps at bit position 3,5,6,7
-i.e. feedback polynomial: x^8 + x^7 + x^6 + x^4 + 1
+The Monarch has taps at registers 8, 6, 5, 4
+i.e. feedback polynomial: x^8 + x^6 + x^5 + x^4 + 1
 """
 from sys import argv
 import collections
@@ -18,20 +18,36 @@ def print_step(clock, data):
 
 
 def calculate_next_step(data):
-    """ Command: calculates the data frame.
+    """ Command: calculates the next data frame.
     Hard-codes the specific taps as used by The Monarch
     """
-    next_value = (data[7] ^ data[6] ^ data[5] ^ data[3]) & 1
+    next_value = (data[7] ^ data[5] ^ data[4] ^ data[3]) & 1
     data.rotate(1)
     data[0] = next_value
 
 
 def run(iterations):
-    bits = collections.deque([1] * 8)
+    """ Command: runs the simulation.
+    If iterations is > 0, runs for the specified number of steps.
+    If iterations is < 1, runs until a full cycle complete.
+    """
+    initial_values = [1] * 8
+    bits = collections.deque(initial_values)
     print_header(bits)
-    for i in range(iterations):
-        print_step(i, bits)
-        calculate_next_step(bits)
+    if iterations > 0:
+        for i in range(iterations):
+            print_step(i, bits)
+            calculate_next_step(bits)
+    else:
+        initial_bits = collections.deque(initial_values)
+        i = 0
+        while True:
+            print_step(i, bits)
+            calculate_next_step(bits)
+            i += 1
+            if bits == initial_bits:
+                print_step(i, bits)
+                break
 
 
 if __name__ == '__main__':
