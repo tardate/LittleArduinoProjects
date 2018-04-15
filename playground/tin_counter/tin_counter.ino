d0006c518e679c68125cfe30e124aaa79c57d5c7
Paul Gallagher <gallagher.paul@gmail.com>
2018-04-15 08:51:08 +0800

    [tin_counter] little code tidy

--- playground/tin_counter/tin_counter.ino
+++ playground/tin_counter/tin_counter.ino
@@ -1,10 +1,16 @@
 /*
-HC-SR04 Ping distance sensor]
-VCC to arduino 5v GND to arduino GND
-Echo to Arduino pin 13 Trig to Arduino pin 12
-Red POS to Arduino pin 11
-Green POS to Arduino pin 10
-560 ohm resistor to both LED NEG and GRD power rail
+  tin_counter
+
+  A project by [novaprimexex](https://github.com/novaprimexex) to count tins as they roll by and detect tins without paper labels.
+
+  For info and circuit diagrams see https://github.com/tardate/LittleArduinoProjects/tree/master/playground/tin_counter
+
+  HC-SR04 Ping distance sensor]
+  VCC to arduino 5v GND to arduino GND
+  Echo to Arduino pin 13 Trig to Arduino pin 12
+  Red POS to Arduino pin 11
+  Green POS to Arduino pin 10
+  560 ohm resistor to both LED NEG and GRD power rail
 */
 
 #define trigPin 13
@@ -13,6 +19,7 @@ Green POS to Arduino pin 10
 #define red 10
 int count = 0;
 int withoutLabel;
+
 void setup() {
   Serial.begin (9600);
   pinMode(trigPin, OUTPUT);
@@ -36,28 +43,25 @@ void loop() {
   duration = pulseIn(echoPin, HIGH);
   distance = (duration/2) / 29.1;
   if (distance < 12) {  // This is where the count happens
-  count = count +1;  
-  Serial.println(count);
-  delay (500);
-   }
-   if(count%4==0) {
-digitalWrite(green, HIGH);
-delay (500);
-digitalWrite(green, LOW);
-   
-   }
-   int contact = analogRead(A0); //reading the voltage on pin A0, to know wether a can is in contact or not
- Serial.println(contact); //prints the value of the contact on the serial monitor(on screen)\
-  
+    count = count +1;
+    Serial.println(count);
+    delay (500);
+  }
+  if(count%4==0) {
+    digitalWrite(green, HIGH);
+    delay (500);
+    digitalWrite(green, LOW);
+  }
+  int contact = analogRead(A0); //reading the voltage on pin A0, to know wether a can is in contact or not
+  Serial.println(contact); //prints the value of the contact on the serial monitor(on screen)\
+
   delay(100);
   if(contact == 0){
     digitalWrite(red,HIGH);//red led on
     delay (500); //delay for half a second
     digitalWrite(red,  LOW); //turn off led
     beep (200);
-    }
-
-
+  }
 }
 
 
@@ -66,6 +70,6 @@ void beep(unsigned char delayms){
                            // experiment to get the best tone
   delay(delayms);          // wait for a delayms ms
   analogWrite(9, 0);       // 0 turns it off
-  delay(delayms);          // wait for a delayms ms   
-}  
+  delay(delayms);          // wait for a delayms ms
+}
 
