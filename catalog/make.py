e5971300b49836ab2f6362ec68d206e824879733
Paul Gallagher <gallagher.paul@gmail.com>
2019-01-01 23:57:41 +0800

    trial an atom feed re #9

--- catalog/make.py
+++ catalog/make.py
@@ -13,6 +13,9 @@ import os
 import re
 import json
 import fnmatch
+from datetime import datetime
+from xml.etree import ElementTree
+from xml.dom import minidom
 
 
 class Catalog(object):
@@ -22,11 +25,11 @@ class Catalog(object):
         path = os.path.dirname(os.path.abspath(__file__))
         self.collection_root = os.path.abspath(os.path.join(path, '..'))
         self.catalog_json = os.path.join(self.collection_root, 'catalog', 'catalog.json')
+        self.catalog_atom = os.path.join(self.collection_root, 'catalog', 'atom.xml')
         self.readme = os.path.join(self.collection_root, 'README.md')
 
     def get_projects_from_readme(self):
         """ Parses the README and returns a list of projects.
-
 
         example README entry:
         | [#197 WienBridgeAudioToneGenerator](./Electronics101/WienBridgeAudioToneGenerator) | **OpAmp, Oscillators**          fixed-frequency tone generator based on a Wien Bridge Oscillator |
@@ -58,7 +61,17 @@ class Catalog(object):
 
     def metadata(self):
         """ Returns the metadata based on .catalog_metadata files. """
-        return map(lambda file: json.load(open(file, 'r')), self.metadata_files())
+        if getattr(self, '_metadata', None) is None:
+            data = map(lambda file: json.load(open(file, 'r')), self.metadata_files())
+            self._metadata = sorted(data, key=lambda k: -int(k['id'].replace('#', '')))
+        return self._metadata
+
+    def get_project_file(self, relative_path, name='.catalog_metadata'):
+        return os.path.join(self.collection_root, relative_path, name)
+
+    def get_project_modified_datetime(self, relative_path):
+        metadata_file = self.get_project_file(relative_path, 'README.md')
+        return datetime.utcfromtimestamp(os.path.getmtime(metadata_file))
 
     def generate(self):
         """ Command: re-writes the catalog file from catalog_metadata. """
@@ -66,10 +79,42 @@ class Catalog(object):
         with open(self.catalog_json, 'w') as f:
             json.dump(self.metadata(), f, indent=4)
 
+    def generate_atom_feed(self):
+        """ Command: re-writes the atom feed file from catalog_metadata. """
+
+        def pretty_write(doc, file):
+            pretty_xml = minidom.parseString(
+                ElementTree.tostring(doc, 'utf-8')
+            ).toprettyxml(indent="  ")
+            with open(file, 'w') as f:
+                f.write(pretty_xml.encode('utf8'))
+
+        print "Writing {}..".format(self.catalog_atom)
+        root = ElementTree.Element("feed", xmlns="http://www.w3.org/2005/Atom")
+        ElementTree.SubElement(root, "title").text = "LEAP: Little Electronic and Arduino Projects"
+        ElementTree.SubElement(root, "subtitle").text = "my collection of electronics projects, many involving an Arduino in one way or another"
+        ElementTree.SubElement(root, "link", href="https://leap.tardate.com/catalog/atom.xml", rel="self")
+        ElementTree.SubElement(root, "link", href="https://leap.tardate.com/")
+        ElementTree.SubElement(root, "id").text = "https://leap.tardate.com/"
+        ElementTree.SubElement(root, "updated").text = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+
+        for entry in self.metadata():
+            url = 'https://github.com/tardate/LittleArduinoProjects/tree/master/{}/README.md'.format(entry['relative_path'])
+            updated_at = self.get_project_modified_datetime(entry['relative_path'])
+            doc = ElementTree.SubElement(root, "entry")
+            ElementTree.SubElement(doc, "id").text = url
+            ElementTree.SubElement(doc, "link", href=url)
+            ElementTree.SubElement(doc, "title").text = u'{} {}'.format(entry['id'], entry['name'])
+            ElementTree.SubElement(doc, "summary").text = u'{}. Keywords: {}'.format(entry['description'], entry['categories'])
+            ElementTree.SubElement(doc, "published").text = updated_at.strftime("%Y-%m-%dT%H:%M:%SZ")
+            ElementTree.SubElement(doc, "updated").text = updated_at.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+        pretty_write(root, self.catalog_atom)
+
     def generate_metadata_files(self):
         """ Command: generates catalog_metadata from README data. """
         for project in self.get_projects_from_readme():
-            metadata_file = os.path.join(self.collection_root, project['relative_path'], '.catalog_metadata')
+            metadata_file = self.get_project_file(project['relative_path'])
             print "Generating {}".format(metadata_file)
             with open(metadata_file, 'w') as f:
                 json.dump(project, f, indent=4)
@@ -78,6 +123,7 @@ class Catalog(object):
         """ Command: rebuild catalog_metadata and catalog.json from README.md. """
         self.generate_metadata_files()
         self.generate()
+        self.generate_atom_feed()
 
 
 if __name__ == '__main__':
