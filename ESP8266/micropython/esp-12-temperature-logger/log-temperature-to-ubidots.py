5e6c9deaaf008abbb323bc37bcacea83b77418b6
Paul Gallagher <gallagher.paul@gmail.com>
2026-01-08 22:28:24 +0800

    [esp-12-temperature-logger] update stats, dashboard, and more robust code (#272)

--- ESP8266/micropython/esp-12-temperature-logger/log-temperature-to-ubidots.py
+++ ESP8266/micropython/esp-12-temperature-logger/log-temperature-to-ubidots.py
@@ -22,17 +22,23 @@ class UbidotsApi():
             "x-ubidots-apikey": UBIDOTS_API_KEY,
             "Content-Type": "application/json"
         }
-        resp = urequests.post(
-            url,
-            headers=headers
-        )
-        print(resp.status_code)
-        print(resp.text)
+        token = None
         try:
-            token = ujson.loads(resp.text).get("token")
-        except Exception:
-            token = None
-        resp.close()
+            resp = urequests.post(
+                url,
+                headers=headers
+            )
+            print(resp.status_code)
+            print(resp.text)
+            try:
+                token = ujson.loads(resp.text).get("token")
+            finally:
+                try:
+                    resp.close()
+                except Exception:
+                    pass
+        except OSError as e:
+            print("OSError on POST:", e, getattr(e, 'args', None))
         return token
 
     def _post_request(self, url, payload):
@@ -40,24 +46,33 @@ class UbidotsApi():
             "X-Auth-Token": self.token,
             "Content-Type": "application/json"
         }
-        resp = urequests.post(
-            url,
-            data=ujson.dumps(payload),
-            headers=headers
-        )
-        print(resp.status_code)
-        print(resp.text)
-        resp.close()
-        if resp.status_code == 401:
-            print("Unauthorized - token may have expired")
-            self._token = None
-        return resp.status_code
+        result = None
+        try:
+            resp = urequests.post(
+                url,
+                data=ujson.dumps(payload),
+                headers=headers
+            )
+            try:
+                result = resp.status_code
+                print(resp.text)
+            finally:
+                try:
+                    resp.close()
+                except Exception:
+                    pass
+            if result == 401:
+                print("Unauthorized - token may have expired")
+        except OSError as e:
+            print("OSError on POST:", e, getattr(e, 'args', None))
+        return result
 
     def post_request(self, url, payload):
         status_code = self._post_request(url, payload)
         if status_code == 401:
-            # retry once with new token
-            print("Retrying with new token...")
+            print("Retrying in 1s with new token...")
+            self._token = None
+            time.sleep(1)
             status_code = self._post_request(url, payload)
         return status_code
 
@@ -71,17 +86,20 @@ class UbidotsApi():
         self.post_request(url, payload)
 
 def read_and_log_temps():
-    ds = ds18x20.DS18X20(onewire.OneWire( Pin(ONEWIRE_PIN)))
+    ds = ds18x20.DS18X20(onewire.OneWire(Pin(ONEWIRE_PIN)))
     roms = ds.scan()
     api = UbidotsApi()
     while True:
-        ds.convert_temp()
-        time.sleep(1)
-        for rom in roms:
-            device_name = 'esp8266-' + ''.join('{:02x}'.format(b) for b in rom)
-            temperature = ds.read_temp(rom)
-            print('Device:', device_name, 'Latest reading:', temperature)
-            api.post_temperature(device_name, temperature)
+        try:
+            ds.convert_temp()
+            time.sleep(1)
+            for rom in roms:
+                device_name = 'esp8266-' + ''.join('{:02x}'.format(b) for b in rom)
+                temperature = ds.read_temp(rom)
+                print('Device:', device_name, 'Latest reading:', temperature)
+                api.post_temperature(device_name, temperature)
+        except Exception as e:
+            print("Error reading temperatures:", e)
         time.sleep(9)
 
 def demo_read_and_log_temps():
